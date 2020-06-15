74c71a15cdb37e8c312651361bbe6b76777a2c24
Orkun Ozekin <orkunozekin@thinkful.com>
2020-06-15 15:42:06 -0500

    inital commit

--- seeds/seed.notes.sql
+++ seeds/seed.notes.sql
@@ -1,60 +1,10 @@
-TRUNCATE notes RESTART IDENTITY CASCADE
+TRUNCATE notes RESTART IDENTITY CASCADE;
 INSERT INTO notes (title, folderId, content) VALUES
 
 
-    ( 
-    "id": "cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Dogs",
-    "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 1"
-    ),
-
-    (
-    "id": "d26e0034-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Cats",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 2"
-    ),
-    (
-    "id": "d26e01a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Pigs",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 3"
-    ),
-    (
-    "id": "d26e0570-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Birds",
-    "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 4"
-    ), 
-    (
-    "id": "d26e0714-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Bears",
-    "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 5"
-    ),
-    (
-    "id": "d26e0854-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Horses",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 5"
-    ),
-    (
-    "id": "d26e0980-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Tigers",
-    "folderId": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 6"
-    ),
-    (
-    "id": "d26e0aac-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Wolves",
-    "folderId": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 7"
-    ),
-    (
-    "id": "d26e0bce-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "title": "Elephants",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "content 8"
-    )
+    ('Dogs', 1, 'content 1'),
+    ('Cats', 2, 'content 2'),
+    ('Zebras', 3, 'content 3');
+ 
+   
 
